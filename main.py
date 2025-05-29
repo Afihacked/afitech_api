384153c7ea6389d9107dea0f463babe0500c4d52
Afitech <59785481+Afihacked@users.noreply.github.com>
2025-05-29 08:04:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ from typing import Optional
 
 app = FastAPI()
 
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse("static/favicon.ico")
+
 BASE_DOWNLOAD_DIR = "downloads"
 os.makedirs(BASE_DOWNLOAD_DIR, exist_ok=True)
 
